7ee9027d33e5d29db5f5af827a17a8d99e377ee0
Zeeshan Faiz <zeshan.faiz@ebitlogix.com>
2022-02-09 03:33:39 +0500

    Amendments

--- controller/users.js
+++ controller/users.js
@@ -1,16 +1,14 @@
 import { db } from "../firebase/firebaseConfig.js"
-// import { v4 as uuidv4 } from "uuid";
 import { collection, addDoc , getDocs, deleteDoc,updateDoc, doc} from "firebase/firestore";
-import { async } from "@firebase/util";
 
 
-let users = [];
 const userRef = collection(db,"users")
 
 export const getUsers = async (req, res) => {
     const Users = await getDocs(userRef);
-    Users.docs.map((doc)=>(users.push({...doc.data(),id:doc.id})))
-    res.send(users);
+    const listUsers = [];
+    Users.docs.map((doc)=>(listUsers.push({...doc.data(),id:doc.id})))
+    res.send(listUsers);
 }
 
 export const createUser = async (req, res) => {
@@ -21,9 +19,10 @@ export const createUser = async (req, res) => {
 
 export const getUser = async (req, res) => {
     const Users = await getDocs(collection(db,"users"));
-    Users.docs.map((doc)=>(users.push({...doc.data(),id:doc.id})))
+    const listUsers = [];
+    Users.docs.map((doc)=>(listUsers.push({...doc.data(),id:doc.id})))
     const { id } = req.params;
-    const FoundUser = users.find((user) => user.id === id);
+    const FoundUser = listUsers.find((user) => user.id === id);
     res.send(FoundUser);
 }
 
@@ -40,18 +39,10 @@ export const deleteUser = async (req, res) => {
         res.send(`User with id ${id} deleted from the database!`)
     }
 }
-const getUserData = async (id) =>{
-    const Users = await getDocs(collection(db,"users"));
-    Users.docs.map((doc)=>(users.push({...doc.data(),id:doc.id})))
-    const FoundUser = users.find((user) => user.id === id);
-    return FoundUser;
-}
 export const updateUser = async (req, res) => {
     const { id } = req.params;
     const conDoc = doc(db, "users", id);
     const { FirstName, LastName, Age } = req.body;
-    // const user = getUserData(id);
-    // debugger
     const newStatus = { FirstName:FirstName,LastName:LastName,Age:Age};
     await updateDoc(conDoc, newStatus);
     res.send(`user ${id} has been updated`)
